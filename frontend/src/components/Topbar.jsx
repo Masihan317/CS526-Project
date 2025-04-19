0268c3330e877494ed79f44baab38ad07bf2cadb
sihanm2 <sihanm2@uw.edu>
2025-04-19 14:56:01 +0800

    added general structure of modal

--- frontend/src/components/Topbar.jsx
+++ frontend/src/components/Topbar.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoIosAddCircleOutline } from "react-icons/io";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 const Topbar = () => {
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className='d-flex justify-content-between align-items-center text-white'>
       <h1>All Tasks</h1>
-      <IoIosAddCircleOutline role='button' size="40" />
+      <IoIosAddCircleOutline role='button' size="40" onClick={handleShow} />
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
