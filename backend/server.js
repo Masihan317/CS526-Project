48e0306ea2f6852c950b37184ff65e82d2abc58a
sihanm2 <sihanm2@uw.edu>
2025-04-19 17:34:31 +0800

    changed default port num on backend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ dotenv.config()
 connectDB()
 const app = express();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
